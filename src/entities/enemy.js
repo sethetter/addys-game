6851d75b37a4e504921ab8a23d738a736831e19b
Seth Etter <sethetter@gmail.com>
2015-08-02 00:23:48 -0500

    More commenting

--- src/entities/enemy.js
+++ src/entities/enemy.js
@@ -7,32 +7,55 @@ var game = require('../game'),
 var _enemies = [];
 
 var Enemy = {
+
+  // Preload enemy sprite
   preload: function() {
     game.load.image('enemy', '../assets/char_01_sized.png');
   },
+
+  /**
+   * Create and return new enemy instance
+   */
   create: function(opts) {
     var enemy = new _Enemy(opts.level);
     _enemies.push(enemy);
     return enemy;
   },
+
+  /**
+   * Perform a random check to see if we want to create a new enemy on the
+   * stage, with increasing chances based on level
+   */
   maybeCreate: function(opts) {
     var check = (util.rand(80 / (opts.level * .6)) === 1)
     return check ? Enemy.create(opts) : false;
   },
+
+  /**
+   * Run update function for each enemy on the stage
+   */
   updateAll: function() {
     _enemies.forEach(function(enemy, idx) {
-      //if (enemy.isDestroyed()) enemies.splice(idx, 1);
+      if (enemy.isDestroyed()) _enemies.splice(idx, 1);
       enemy.update();
     });
   }
 };
 
+/**
+ * Create a new enemy just above the top of the stage, and a random spot along
+ * the x axis. Set the level and the velocity which is based on level
+ */
 function _Enemy(level) {
   this._obj = game.add.sprite(util.rand(30, game.world.width - 30), -50, 'enemy');
   this.level = level;
   this.velocity = _getVelocity(this.level);
 }
 
+/**
+ * If it goes off the stage, destroy it, otherwise advanced it downwards at the
+ * velocity determined by level
+ */
 _Enemy.prototype.update = function() {
   if (_checkOutOfBounds(this._obj.y)) this._obj.destroy();
   this._obj.y += this.velocity;
--- src/entities/thing.js
+++ src/entities/thing.js
@@ -7,16 +7,30 @@ var game = require('../game'),
 var _things = [];
 
 var Thing = {
+
+  /**
+   * Used to know if we wait longer to spawn another thing
+   */
   _waitFlag: false,
 
+  // Preload thing sprite
   preload: function() {
     game.load.image('thing', '../assets/char_02_sized.png');
   },
+
+  /**
+   * Create and return a new thing object
+   */
   create: function(opts) {
     var thing = new _Thing(opts.level);
     _things.push(thing);
     return thing;
   },
+
+  /**
+   * Only create a new thing if the waitFlag is false. Once one is created, the
+   * flag is flipped, and will be turned back off after 2 seconds
+   */
   maybeCreate: function(opts) {
     if (Thing._waitFlag) return false;
 
@@ -26,20 +40,32 @@ var Thing = {
 
     return Thing.create(opts);
   },
+
+  /**
+   * Run the update function for each thing on the screen
+   */
   updateAll: function() {
     _things.forEach(function(thing, idx) {
-      //if (thing.isDestroyed()) enemies.splice(idx, 1);
+      if (thing.isDestroyed()) _things.splice(idx, 1);
       thing.update();
     });
   }
 };
 
+/**
+ * Create a new thing just above the top of the stage, and a random spot along
+ * the x axis. Set the level and the velocity which is based on level
+ */
 function _Thing(level) {
   this._obj = game.add.sprite(util.rand(30, game.world.width - 30), -50, 'thing');
   this.level = level;
   this.velocity = _getVelocity(this.level);
 }
 
+/**
+ * If it goes off the stage, destroy it, otherwise advanced it downwards at the
+ * velocity determined by level
+ */
 _Thing.prototype.update = function() {
   if (_checkOutOfBounds(this._obj.y)) this._obj.destroy();
   this._obj.y += this.velocity;
